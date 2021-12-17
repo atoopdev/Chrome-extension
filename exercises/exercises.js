1c7e513d1561e3007de4cfc325706189f9a9f8f7
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-17 10:12:01 -0800

    adding event listeners

---
+++ exercises/exercises.js
@@ -0,0 +1,4 @@
+let boxAction = document.getElementById("box");
+boxAction.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function saveInput(){
-    console.log("Button clicked!");
-}
+
+// add event listener to Save Input button
+//replaces use of "onclick" in html tags
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from event listener.")
+})

26f73ebac09da911f60b92d8617c093279fd0b58
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-17 11:57:54 -0800

    grab input text and store on button click

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -2,3 +2,8 @@ let boxAction = document.getElementById("box");
 boxAction.addEventListener("click", function(){
     console.log("I want to open the box!")
 })
+
+// // not to be reassigned
+// const playerName = "Per";
+// // should be reassigned later in code
+// let credits = 45;
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let myLeads = [];
+// const prevents variable from being reassigned
+const inputEL = document.getElementById("input-el");
 
 // add event listener to Save Input button
 //replaces use of "onclick" in html tags
-let inputBtn = document.getElementById("input-btn");
+const inputBtn = document.getElementById("input-btn");
+
 inputBtn.addEventListener("click", function(){
     console.log("Button clicked from event listener.")
+    // console.log(inputEL.value);
+
+    // take field input and push into leads array
+    myLeads.push(inputEL.value);
+    console.log(myLeads);
 })

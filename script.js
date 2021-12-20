6c44be9a89331fc26e8fc1ab5e235d8ce165f3f7
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-20 13:50:10 -0800

    render saved items to screen using better performance

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 // const prevents variable from being reassigned
 const inputEL = document.getElementById("input-el");
 
@@ -7,15 +7,40 @@ const inputEL = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEL = document.getElementById("ul-el");
 
+
+
 inputBtn.addEventListener("click", function () {
     console.log("Button clicked from event listener.");
 
     // take field input and push into leads array
     myLeads.push(inputEL.value);
     console.log(myLeads);
+    renderLeads();
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    // console.log(myLeads[i]);
-    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
+// -----------------------------renderLeads()-------------------------------------
+function renderLeads() {
+    // hold all html for list items
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+        // console.log(myLeads[i]);
+
+        // one way to output to screen - may be better because easily human readable
+        // add list items to variable for improved performance
+        listItems += "<li>" + myLeads[i] + "</li>";
+
+        // another method - may encounter so good to know
+        // create html element
+        // const li = document.createElement("li");
+        // // set text content
+        // li.textContent = myLeads[i];
+        // // append closing tag
+        // ulEL.append(li);
+
+    }
+    // output after for loop for best performance
+    ulEL.innerHTML = listItems;
 }
+
+// -----------------------------------------------------------------------------------

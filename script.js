2d154adfa02bf4f73a1973365c92d11211a8aaf2
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-20 10:15:36 -0800

    render array contents to screen

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ const inputEL = document.getElementById("input-el");
 // add event listener to Save Input button
 //replaces use of "onclick" in html tags
 const inputBtn = document.getElementById("input-btn");
+const ulEL = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from event listener.")
-    // console.log(inputEL.value);
+inputBtn.addEventListener("click", function () {
+    console.log("Button clicked from event listener.");
 
     // take field input and push into leads array
     myLeads.push(inputEL.value);
     console.log(myLeads);
 })
 
-for(let i=0; i<myLeads.length; i++){
-    console.log(myLeads[i]);
+for (let i = 0; i < myLeads.length; i++) {
+    // console.log(myLeads[i]);
+    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
 }

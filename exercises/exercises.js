3f7465ffaee58c6b323f48d86b2b7594e371e427
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-20 12:27:18 -0800

    .innerHTML exercises

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,7 +1,16 @@
-let boxAction = document.getElementById("box");
-boxAction.addEventListener("click", function(){
-    console.log("I want to open the box!")
-})
+buyBtn = document.getElementById("container");
+
+buyBtn.innerHTML = "<button onclick='buy()'>Buy!</button>";
+
+function buy() {
+    buyBtn.innerHTML += "<p>Thank you for buying!</p>";
+}
+
+
+// let boxAction = document.getElementById("box");
+// boxAction.addEventListener("click", function(){
+//     console.log("I want to open the box!")
+// })
 
 // // not to be reassigned
 // const playerName = "Per";

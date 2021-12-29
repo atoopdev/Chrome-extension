e461cb34b1e8c5ccfbbdfe70fb6fec80da3bd922
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-29 13:51:43 -0800

    parameters exercises

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,14 +1,36 @@
+function add(num1, num2){
+    return num1+num2;
+}
+
+sum = add(9,102);
+console.log(sum);
+
+
+// function parameters
+const welcomeEL = document.getElementById("welcome-el");
+
+function greetUser(greeting, name){
+    welcomeEL.textContent = `${greeting} ${name}!`;
+
+}
+
+greetUser("Hello","May");
+
+
+
+
+
 // practice template strings/literals
 
-const recipient = "James";
-const sender = "Amanda";
+// const recipient = "James";
+// const sender = "Amanda";
 
-const email = `
-    Hey ${recipient}! 
-    How is it going? 
-    Cheers, ${sender}`;
+// const email = `
+//     Hey ${recipient}! 
+//     How is it going? 
+//     Cheers, ${sender}`;
 
-console.log(email);
+// console.log(email);
 
 
 

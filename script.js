10733afdba108322ab8f3efec554efb32212ba37
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-29 10:41:59 -0800

    updated renderLeads() to render()
    removed internal use of hardcoded global array and switched to passed variable to improve reusability

--- script.js
+++ script.js
@@ -18,9 +18,41 @@ console.log(leadsFromLocalStorage);
 // if leadsFromLocalStorage has content, render to screen
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 } else{
     console.log("leadsFromLocalStorage not truthy");
+}
+
+// -----------------------------renderLeads()-------------------------------------
+function render(leads) {
+    // hold all html for list items
+    let listItems = "";
+
+    for (let i = 0; i < leads.length; i++) {
+        // console.log(leads[i]);
+
+        // one way to output to screen - may be better because easily human readable
+        // add list items to variable for improved performance
+        // listItems += "<li><a href=" + myLeads[i] + " target='blank'>" + myLeads[i] + "</a ></li > ";
+        listItems += `
+        <li>
+            <a href="${leads[i]}" target='blank'>
+                ${leads[i]}
+                </a >
+                </li > 
+            `;
+
+        // another method - may encounter so good to know
+        // create html element
+        // const li = document.createElement("li");
+        // // set text content
+        // li.textContent = leads[i];
+        // // append closing tag
+        // ulEL.append(li);
+
+    }
+    // output after for loop for best performance
+    ulEL.innerHTML = listItems;
 }
 
 // --------------------Save Button---------------------------------------------
@@ -38,8 +70,8 @@ inputBtn.addEventListener("click", function () {
 
     // clear input field
     inputEL.value = "";
-    // console.log(myLeads);
-    renderLeads();
+    // console.log(leads);
+    render(myLeads);
 
     console.log("Output from local storage:", localStorage.getItem("myLeads"));
 
@@ -54,43 +86,6 @@ deleteBtn.addEventListener("click", function() {
     // clear everything
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 }
 )
-
-
-
-
-// -----------------------------renderLeads()-------------------------------------
-function renderLeads() {
-    // hold all html for list items
-    let listItems = "";
-
-    for (let i = 0; i < myLeads.length; i++) {
-        // console.log(myLeads[i]);
-
-        // one way to output to screen - may be better because easily human readable
-        // add list items to variable for improved performance
-        // listItems += "<li><a href=" + myLeads[i] + " target='blank'>" + myLeads[i] + "</a ></li > ";
-        listItems += `
-        <li>
-            <a href="${myLeads[i]}" target='blank'>
-                ${myLeads[i]}
-                </a >
-                </li > 
-            `;
-
-        // another method - may encounter so good to know
-        // create html element
-        // const li = document.createElement("li");
-        // // set text content
-        // li.textContent = myLeads[i];
-        // // append closing tag
-        // ulEL.append(li);
-
-    }
-    // output after for loop for best performance
-    ulEL.innerHTML = listItems;
-}
-
-// -----------------------------------------------------------------------------------
